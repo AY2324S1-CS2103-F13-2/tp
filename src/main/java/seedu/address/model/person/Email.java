c3b96fd02b3b23156aa77c6e9592bdf9b4da4a3e
remuslum <57741494+remuslum@users.noreply.github.com>
2023-11-08 06:53:11 +0800

    Modified regex pattern for email

--- src/main/java/seedu/address/model/person/Email.java
+++ src/main/java/seedu/address/model/person/Email.java
@@ -15,17 +15,17 @@ public class Email {
             + "1. The local-part should only contain alphanumeric characters and these special characters, excluding "
             + "the parentheses, (" + SPECIAL_CHARACTERS + "). The local-part may not start or end with any special "
             + "characters.\n"
-            + "2. This is followed by a '@' and then a domain name. The domain name is made up of domain labels "
-            + "separated by periods.\n"
-            + "The domain name must:\n"
-            + "    - end with a domain label at least 2 characters long\n"
-            + "    - have each domain label start and end with alphanumeric characters\n"
-            + "    - have each domain label consist of alphanumeric characters, separated only by hyphens, if any.";
+            + "2. This is followed by a '@' and then the domain name, which is gmail.com.\n"
+            + "The domain name can only be gmail.com as only company emails are allowed to be "
+            + "entered into the system.";
+
+    private static final String ALPHANUMERIC_NO_UNDERSCORE = "[^\\W_]+"; // alphanumeric characters except underscore
+    private static final String LOCAL_PART_REGEX = "^" + ALPHANUMERIC_NO_UNDERSCORE + "([" + SPECIAL_CHARACTERS + "]"
+            + ALPHANUMERIC_NO_UNDERSCORE + ")*";
+    private static final String DOMAIN_REGEX = "gmail\\.com$";
+    public static final String VALIDATION_REGEX = LOCAL_PART_REGEX + "@" + DOMAIN_REGEX;
 
     public final String value;
-    // Validation Regex for Email
-    private static final String VALIDATION_REGEX = "\\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}\\b";
-
 
     /**
      * Constructs an {@code Email}.
--- src/test/java/seedu/address/testutil/TypicalPersons.java
+++ src/test/java/seedu/address/testutil/TypicalPersons.java
@@ -27,42 +27,42 @@ import seedu.address.model.person.Person;
 public class TypicalPersons {
 
     public static final Person ALICE = new PersonBuilder().withName("Alice Pauline")
-            .withAddress("123, Jurong West Ave 6, #08-111").withEmail("alice@example.com")
+            .withAddress("123, Jurong West Ave 6, #08-111").withEmail("alice@gmail.com")
             .withPhone("94351253").withSalary("10000").withClaimBudget("5000").withDepartment("Engineering")
             .withDob("2000-01-01").withLeave("111101010101").build();
     public static final Person BENSON = new PersonBuilder().withName("Benson Meier")
             .withAddress("311, Clementi Ave 2, #02-25")
-            .withEmail("johnd@example.com").withPhone("98765432")
+            .withEmail("johnd@gmail.com").withPhone("98765432")
             .withSalary("100").withClaimBudget("5").withDepartment("Sales")
             .withDob("1997-05-05").withLeave("010101010101").build();
     public static final Person CARL = new PersonBuilder().withName("Carl Kurz").withPhone("95352563")
-            .withEmail("heinz@example.com").withAddress("wall street")
+            .withEmail("heinz@gmail.com").withAddress("wall street")
             .withSalary("1000000000000").withClaimBudget("500000").withDepartment("Executive")
             .withDob("1991-12-31").withLeave("000101010001").build();
     public static final Person DANIEL = new PersonBuilder().withName("Daniel Meier").withPhone("87652533")
-            .withEmail("cornelia@example.com").withAddress("10th street")
+            .withEmail("cornelia@gmail.com").withAddress("10th street")
             .withSalary("23232323232").withClaimBudget("19191919").withDepartment("Marketing")
             .withDob("1969-07-27").withLeave("111111111111").build();
     public static final Person ELLE = new PersonBuilder().withName("Elle Meyer").withPhone("9482224")
-            .withEmail("werner@example.com").withAddress("michegan ave")
+            .withEmail("werner@gmail.com").withAddress("michegan ave")
             .withSalary("3000").withClaimBudget("200").withDepartment("HR")
             .withDob("1999-06-17").withLeave("000000000001").build();
     public static final Person FIONA = new PersonBuilder().withName("Fiona Kunz").withPhone("9482427")
-            .withEmail("lydia@example.com").withAddress("little tokyo")
+            .withEmail("lydia@gmail.com").withAddress("little tokyo")
             .withSalary("100000").withClaimBudget("1").withDepartment("Finance")
             .withDob("2002-02-27").withLeave("000000100000").build();
     public static final Person GEORGE = new PersonBuilder().withName("George Best").withPhone("9482442")
-            .withEmail("anna@example.com").withAddress("4th street")
+            .withEmail("anna@gmail.com").withAddress("4th street")
             .withSalary("10000").withClaimBudget("5000").withDepartment("Engineering")
             .withDob("2005-09-30").withLeave("111111000000").build();
 
     // Manually added
     public static final Person HOON = new PersonBuilder().withName("Hoon Meier").withPhone("8482424")
-            .withEmail("stefan@example.com").withAddress("little india")
+            .withEmail("stefan@gmail.com").withAddress("little india")
             .withSalary("10000").withClaimBudget("5000").withDepartment("Engineering")
             .withDob("2000-01-01").withLeave("000000000000").build();
     public static final Person IDA = new PersonBuilder().withName("Ida Mueller").withPhone("8482131")
-            .withEmail("hans@example.com").withAddress("chicago ave")
+            .withEmail("hans@gmail.com").withAddress("chicago ave")
             .withSalary("10000").withClaimBudget("5000").withDepartment("Engineering")
             .withDob("2000-01-01").withLeave("111000111000").build();
 
